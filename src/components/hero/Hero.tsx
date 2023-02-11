5cd07cf48f5351f816aa1df61b44659aa38518d0
Adriel Santana <diel_san@hotmail.com>
2023-02-11 18:29:50 -0300

    animação do hero finalizada

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Container } from "react-bootstrap";
+
+import RandomPokemon from "./RandomPokemon";
 
 import pokeball from "/public/images/hero/pokeball-hero.png";
 
@@ -10,15 +13,7 @@ const Hero = () => {
       <section className="hero-section">
         <Container className="hero-container">
           <Image className="pokeball-hero" alt="Pokeball" src={pokeball} />
-
-          <span className="random-pokemon-span">
-            <Image
-              alt="Random Pokemon"
-              src={"/images/hero/mewtwo.gif"}
-              className="random-pokemon"
-              fill
-            />
-          </span>
+          <RandomPokemon/>
         </Container>
       </section>
     </>
